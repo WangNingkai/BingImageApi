55c762b3bc18d0e7bf2f21ccac59f5c9cb1f9991
WangNingkai <1655586865@qq.com>
2020-07-27 12:47:41 +0800

    .

--- spider.py
+++ spider.py
@@ -4,10 +4,10 @@ from requests import get
 
 
 def fetchImage():
-    api_url = r'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    api_url = r'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
     api = get(api_url)
     json_data = json.loads(api.text)
-    pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
+    pic_url = r'https://s.cn.bing.net{0}'.format(json_data['images'][0]['url'])
     start_date = json_data['images'][0]['enddate']
     print(start_date)
     open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
