07225fdce14501942e1e976dc1ae7b04f93629cb
WangNingkai <1655586865@qq.com>
2020-07-28 14:41:29 +0800

    update spider

--- spider.py
+++ spider.py
@@ -7,15 +7,18 @@ def fetchImage():
     api_url = r'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
     api = get(api_url)
     json_data = json.loads(api.text)
-    pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
-    start_date = json_data['images'][0]['startdate']
-    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
-    pic = get(pic_url, stream=True)
-    if (pic.status_code == 200):
-        open(r'./images/{0}.png'.format(start_date), 'wb').write(pic.content)
-        print('Create Image Success!')
-    else:
-        print('Create Image Faild!')
+    images = json_data['images']
+    for item in images:
+        pic_url = r'https://www.bing.com{0}'.format(item['url'])
+        start_date = item['enddate']
+        open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+        pic = get(pic_url, stream=True)
+        if (pic.status_code == 200):
+            open(r'./images/{0}.png'.format(start_date),
+                 'wb').write(pic.content)
+            print(r'Create {0} Image Success!'.format(start_date))
+        else:
+            print(r'Create {0} Image Failed!'.format(start_date))
 
 
 if __name__ == "__main__":
